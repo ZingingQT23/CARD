b143c0880ccf4ba01ba560f210f040860e688eee
Dylan Kenyon <Dylan.Dean.Kenyon@Gmail.com>
2018-01-09 22:33:52 -0500

    Fixed bug where defenders spawned when hitting unpause button

--- Assets/Scripts/PauseBtn.cs
+++ Assets/Scripts/PauseBtn.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class PauseBtn : MonoBehaviour {
 
 	public GameObject unpauseButton;
-	
+	public GameObject coreGame;
+
     public void Pause(){
+		coreGame.SetActive (false);
         unpauseButton.SetActive(true);
         Time.timeScale = 0.0000001f;
         gameObject.SetActive(false);
     }
     
+	private void ActivateCoreGame(){
+		coreGame.SetActive (true);
+	}
+
     public void Unpause(){
         unpauseButton.SetActive(false);
         Time.timeScale = 1.0f;
         gameObject.SetActive(true);
+		Invoke("ActivateCoreGame", 0.1f);
     }
 }
